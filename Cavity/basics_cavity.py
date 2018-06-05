c450ca15875cb661dc28b9ee03b7a842db0fb1fd
Unknown <lars.kohfahl@physik.tu-darmstadt.de>
2018-06-05 14:39:45 +0200

    Script for basic Cavity calculations
    Script to do basic cavity calculations using OOP in python.

---
+++ Cavity/basics_cavity.py
@@ -0,0 +1,55 @@
+# Import section
+import scipy.constants
+import numpy as np
+
+
+# Definitions
+micro = 1e-6
+nano = 1e-9
+c = scipy.constants.c
+
+# Class definitions:
+
+
+class cavity:
+    def __init__(self, length, R1, R2, n, wavelength):
+        self.length = length
+        self.R1 = R1  # Reflectivity of first mirror
+        self.R2 = R2  # Reflectivity second mirror
+        self.n = n
+
+    def FSR(self):
+        return c / (2 * self.length * self.n)
+
+    def Finesse(self):
+        F = (scipy.constants.pi * (self.R1 * self.R2)**(1 / 4)) / (1 - np.sqrt(self.R1 * self.R2))
+        return F
+
+    def Coeff_of_Finesse(self):
+        F = 4 * np.sqrt(self.R1 * self.R2) / ((1 - np.sqrt(self.R1 * self.R2))**2)
+        return F
+
+    def linewidth_FWHM(self):
+        FWHM = self.FSR() / self.Finesse()
+        return FWHM
+
+    def max_Transmission(self):
+        T_max = 1 / (1 + self.Coeff_of_Finesse())
+        return T_max
+
+    def Internal_Intensity(self, I_0):
+        I = I_0 * (1 + np.sqrt(self.R1 * self.R2) + 2 * (self.R1 * self.R2)**(1 / 4)) / (1 - np.sqrt(self.R1 * self.R2))
+        return I
+
+
+R_780 = 0.999908
+R_960 = 0.99985
+lambda_780 = 780 * nano
+n_vac = 1
+
+SLS_Cavity_780 = cavity(0.1, R_780, R_780, n_vac, lambda_780)
+print(SLS_Cavity_780.FSR())
+print(SLS_Cavity_780.Finesse())
+print(SLS_Cavity_780.linewidth_FWHM())
+print(SLS_Cavity_780.max_Transmission())
+print(SLS_Cavity_780.Internal_Intensity(10 * micro))
