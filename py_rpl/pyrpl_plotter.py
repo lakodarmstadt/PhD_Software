4928529d9c630ac9ec4e81dd2ecdac69cbc896dc
Lars Kohfahl <lars.kohfahl@physik.tu-darmstadt.de>
2018-09-20 14:35:07 +0200

    Plotter for data saved from pyrpl-userinterface
    Can now handle both xy-mode and time-mode plots

--- py_rpl/pyrpl_plotter.py
+++ py_rpl/pyrpl_plotter.py
@@ -6,27 +6,47 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-curve_1 = pickle.load(open("/home/lars/pyrpl_user_dir/curve/2.dat", "rb"))
-data_curve_1 = curve_1[2]
+
+def load_data(filename):
+    data_all = pickle.load(open(filename, "rb"))
+    print(data_all[1])
+    return data_all[1], data_all[2]
 
 
-# Note that using plt.subplots below is equivalent to using
-# fig = plt.figure() and then ax = fig.add_subplot(111)
-fig, ax = plt.subplots()
-ax.plot(data_curve_1[0], data_curve_1[1])
-
-ax.set(xlabel='x', ylabel='y',
-       title='About as simple as it gets, folks')
-ax.grid()
-
-# fig.savefig("test.png")
-plt.show()
-
-print(len(curve_1))
+# filename1 = "/home/lars/pyrpl_user_dir/curve/1.dat"
+# filename2 = "/home/lars/pyrpl_user_dir/curve/2.dat"
 
 
-# curve_test = pyrpl.curvedb.CurveDB(name='test')
-# curve_test.data = (0, 1)
+def pyrpl_plotter(filename1, filename2, title_name, save_filename):
 
+    dict_1, data_1 = load_data(filename1)
+    dict_2, data_2 = load_data(filename2)
 
-# print(curve_test.params)
+    if np.shape(data_1) != np.shape(data_2):
+        raise Exception('Input data has not equal dimension')
+
+    if dict_1['xy_mode']:
+        # Note that using plt.subplots below is equivalent to using
+        # fig = plt.figure() and then ax = fig.add_subplot(111)
+        fig, ax = plt.subplots()
+        ax.plot(data_1[1], data_2[1])
+
+        ax.set(xlabel=dict_1['input1'], ylabel=dict_2['input2'],
+               title=title_name)
+        ax.grid()
+
+        fig.savefig(save_filename)
+        plt.show()
+    else:
+        # Note that using plt.subplots below is equivalent to using
+        # fig = plt.figure() and then ax = fig.add_subplot(111)
+        fig, ax = plt.subplots()
+        ax.plot(data_1[0], data_1[1])
+        ax.plot(data_2[0], data_2[1])
+
+        ax.set(xlabel='Time [s]', ylabel=dict_2['input2'],
+               title=title_name)
+        ax.grid()
+
+        fig.savefig(save_filename)
+        plt.show()
